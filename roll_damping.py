2abb684a925181b996acfc6319b53abdf62d5a34
martinlarsalbert <marale@kth.se>
2020-07-08 15:44:40 +0200

    Yea now it is working (locally at least)

--- roll_damping.py
+++ roll_damping.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 # 3rd party modules
 from flask import make_response, abort
+import numpy as np
 
 # My modules
 from rolldecayestimators.polynom_estimator import Polynom
@@ -15,27 +16,7 @@ from rolldecayestimators.polynom_estimator import Polynom
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
-
-# Data to serve with our API
-PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp(),
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp(),
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp(),
-    },
-}
-
-def predict(lpp):
+def predict(lpp, beam, T, BK_L, BK_B, OG, omega0_hat, C_b, A_0, V, phi_a):
     """
     This function responds to a request for /api/roll_damping
     To predict the roll damping of a ship.
@@ -44,16 +25,28 @@ def predict(lpp):
     :return:      ship roll damping prediction
     """
 
-    damping = {}
-
-    file_path = 'polynom_complex.sym'
-    inputs={
-
+    file_path = 'static/models/polynom_complex.sym'
+    inputs = {
+        'beam': beam/lpp,
+        'T': T/lpp,
+        'BK_L': BK_L/lpp,
+        'BK_B': BK_B/lpp,
+        'OG': OG/lpp,
+        'omega0_hat': omega0_hat,
+        'C_b': C_b,
+        'A_0': A_0,
+        'V': V/np.sqrt(lpp),
+        'phi_a': phi_a,
     }
-    make_prediction(file_path=file_path, inputs=inputs)
+    damping = make_prediction(file_path=file_path, inputs=inputs)
+
+    results = {
+        'method':file_path,
+        'B_e_hat':damping,
+    }
 
     if lpp>0:
-        damping['lpp']=lpp
+        results['lpp']=lpp
 
     # otherwise, nope, not found
     else:
@@ -62,7 +55,7 @@ def predict(lpp):
         )
 
 
-    return damping
+    return results
 
 
 def make_prediction(file_path:str, inputs:dict):
